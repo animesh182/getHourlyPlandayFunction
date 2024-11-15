725aa82cee7d1ee5c9e3958204a286832d81c5cd
Sajjan <sajjanpaudel811@gmail.com>
2024-11-15 12:03:19 +0545

    minor additional changes

--- PlandayHourly/constants.py
+++ PlandayHourly/constants.py
@@ -13,6 +13,7 @@ restaurantnames = {
     "Drift Olav Kyrres Gate 27 AS": "Bergen",
     "Fisketorget Stavanger AS": "Restaurant",
     "Alex Kiellands": "Alexander Kielland",
+    "Bjørvika": "Bjørvika",
     # "Servering": "Restaurantdrift AS",  # there are two departments in Broker Servering and kjokken
     # "Kjøkken": "Restaurantdrift AS",
 }
@@ -21,10 +22,10 @@ company_data = {
         "client_id": "806cc44e-6e2f-4809-b955-65a96975fa52",
         "refresh_token": "F_4jQsoAeEqcx_saD84lrQ",
     },
-    "Fisketorget": {
-        "client_id": "ca4d6275-b929-40b7-95a3-0fa45dac1d01",
-        "refresh_token": "NzTsFtfzvE28jLmex3gHnw",
-    },
+    # "Fisketorget": {
+    #     "client_id": "ca4d6275-b929-40b7-95a3-0fa45dac1d01",
+    #     "refresh_token": "NzTsFtfzvE28jLmex3gHnw",
+    # },
     # "The Broker": {
     #     "client_id": "b63a8270-4d8d-4b42-a427-6a6d33dffc4d",
     #     "refresh_token": "0IcvJxpX3k6VeeEWN14QxA",
